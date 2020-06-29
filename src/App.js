b8c499b50a3710644ed616e9ad8bbcc12ee50659
Cesar Orozco Zamora <35977775+caesarorz@users.noreply.github.com>
2020-06-29 11:50:47 -0600

    Revert "Feature"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bella Kiarita, las mas bonita.
+          Kiaritas
         </p>
         <a
           className="App-link"
